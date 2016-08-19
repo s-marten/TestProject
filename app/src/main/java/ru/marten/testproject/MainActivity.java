a9d4492aba852088f554d39f58814b20b4c84161
marten <s-marten@octopus.name>
2016-08-19 03:16:20 +0300

    async test

--- app/src/main/java/ru/marten/testproject/MainActivity.java
+++ app/src/main/java/ru/marten/testproject/MainActivity.java
@@ -2,12 +2,41 @@ package ru.marten.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import ru.marten.testproject.netutils.CustomAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView status_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        status_text = (TextView) findViewById(R.id.status_text);
+        Button testBtn = (Button) findViewById(R.id.start_button);
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                asyncTest();
+            }
+        });
+    }
+
+    public void setText() {
+        status_text.setText("Got it");
+    }
+
+    public void asyncTest() {
+        CustomAsyncTask task = new CustomAsyncTask();
+        try {
+            task.setPostExecuteLogic(this.getClass().getMethod("setText", null), this);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        task.execute();
     }
 }
---
+++ app/src/main/java/ru/marten/testproject/netutils/CustomAsyncTask.java
@@ -0,0 +1,52 @@
+package ru.marten.testproject.netutils;
+
+import android.os.AsyncTask;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by marten on 19.08.16.
+ */
+public class CustomAsyncTask extends AsyncTask<Object, Object, Object> {
+
+    private Method method;
+    private Object object;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Object doInBackground(Object... objects) {
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Object[] values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        try {
+            method.invoke(object);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        super.onPostExecute(o);
+    }
+
+    public void setPostExecuteLogic(Method method, Object object) {
+       this.method = method;
+       this.object = object;
+    }
+
+
+}
